50ab96fbcdbe9cbebd4e5d79218b1cd1d9e0c172
toango123zx <toango123zx@gmail.com>
2025-02-10 09:55:46 +0700

    feat: login endpoint

--- tripBackend/src/configs/jwt.config.ts
+++ tripBackend/src/configs/jwt.config.ts
@@ -2,7 +2,7 @@ export const jwtConfig = {
 	expiresIn: process.env.EXPIRES_IN_ACCESS_KEY || '1h',
 	secret:
 		process.env.JWT_SECRET_ACCESS_KEY ||
-		(() => {
+		((): string => {
 			if (process.env.NODE_ENV === 'production') {
 				throw new Error('JWT_SECRET_ACCESS_KEY must be set in production');
 			}
