1019b568f01b3dd9937d0c023ef621b70c99d48f
toango123zx <toango123zx@gmail.com>
2025-05-11 13:17:01 +0700

    feat(discount-type): implement retrieval of discount types with pagination and filtering

--- tripBackend/src/modules/discountType/discountType.module.ts
+++ tripBackend/src/modules/discountType/discountType.module.ts
@@ -3,12 +3,14 @@ import { CqrsModule } from '@nestjs/cqrs';
 
 import { DatabaseModule } from '../database/database.module';
 
+import { DiscountTypeController } from './discountType.controller';
 import { DiscountTypeRepository } from './discountType.repository';
+import { DiscountTypeQueryHandlers } from './queries/handlers';
 
 @Module({
 	imports: [CqrsModule, DatabaseModule],
-	controllers: [],
-	providers: [DiscountTypeRepository],
+	controllers: [DiscountTypeController],
+	providers: [DiscountTypeRepository, ...DiscountTypeQueryHandlers],
 	exports: [],
 })
 export class DiscountTypeModule {}
