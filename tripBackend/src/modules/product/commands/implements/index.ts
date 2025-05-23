66178071d7a735a4a70dafdd37e1d87404fa71a6
toango123zx <toango123zx@gmail.com>
2025-05-23 11:10:15 +0700

    feat(productRate): implement create product rate functionality with command, handler, and DTOs

--- tripBackend/src/modules/product/commands/implements/index.ts
+++ tripBackend/src/modules/product/commands/implements/index.ts
@@ -1,4 +1,5 @@
 export * from './createProduct.command';
+export * from './createProductRateByProductId.command';
 export * from './creatProductScheduleByProductId.command';
 export * from './deleteProductByProductId.command';
 export * from './updateProductInformationByProductId.command';
