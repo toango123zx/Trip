bc6a5c8462d79699029b76c77810280272a2564f
toango123zx <toango123zx@gmail.com>
2025-06-02 21:01:26 +0700

    feat(product): add discount information to GetProductsResponseDto and update product fetching logic

--- tripBackend/src/modules/product/dtos/responses/getProducts.response.ts
+++ tripBackend/src/modules/product/dtos/responses/getProducts.response.ts
@@ -21,6 +21,36 @@ export class GetProductsResponseDto {
 		image: string;
 		status: string;
 	};
+	schedule: {
+		id: string;
+		startTime: Date;
+		endTime: Date;
+		price: number;
+		booked: number;
+		startOrder: Date;
+		endOrder: Date;
+		createAt: Date;
+		updateAt: Date;
+		deletedAt: Date;
+		status: string;
+	};
+	discount: {
+		id: string;
+		name: string;
+		code: string;
+		description: string;
+		startTime: Date;
+		endTime: Date;
+		value: number;
+		quantity: number;
+		point: number;
+		applited: number;
+		stackable: boolean;
+		createAt: Date;
+		updateAt: Date;
+		deletedAt: Date;
+		status: string;
+	};
 	createAt: Date;
 	updateAt: Date;
 	deletedAt: Date;
@@ -47,6 +77,56 @@ export class GetProductsResponseDto {
 			image: product.supplier.user.image,
 			status: product.supplier.user.status,
 		};
+		this.schedule =
+			product.productSchedule && product.productSchedule.length > 0
+				? {
+						id: product.productSchedule[0].id,
+						startTime: product.productSchedule[0].startTime,
+						endTime: product.productSchedule[0].endTime,
+						price: product.productSchedule[0].price,
+						booked: product.productSchedule[0].booked,
+						startOrder: product.productSchedule[0].startOrder,
+						endOrder: product.productSchedule[0].endOrder,
+						createAt: product.productSchedule[0].createAt,
+						updateAt: product.productSchedule[0].updateAt,
+						deletedAt: product.productSchedule[0].deletedAt,
+						status: product.productSchedule[0].status,
+					}
+				: undefined;
+		this.discount =
+			product.productSchedule &&
+			product.productSchedule.length > 0 &&
+			product.productSchedule[0].infoBill &&
+			product.productSchedule[0].infoBill.length > 0
+				? {
+						id: product.productSchedule[0].infoDiscount[0].discount.id,
+						name: product.productSchedule[0].infoDiscount[0].discount.name,
+						code: product.productSchedule[0].infoDiscount[0].discount.code,
+						description:
+							product.productSchedule[0].infoDiscount[0].discount
+								.description,
+						startTime:
+							product.productSchedule[0].infoDiscount[0].discount.startTime,
+						endTime:
+							product.productSchedule[0].infoDiscount[0].discount.endTime,
+						value: product.productSchedule[0].infoDiscount[0].discount.value,
+						quantity:
+							product.productSchedule[0].infoDiscount[0].discount.quantity,
+						point: product.productSchedule[0].infoDiscount[0].discount.point,
+						applited:
+							product.productSchedule[0].infoDiscount[0].discount.applited,
+						stackable:
+							product.productSchedule[0].infoDiscount[0].discount.stackable,
+						createAt:
+							product.productSchedule[0].infoDiscount[0].discount.createAt,
+						updateAt:
+							product.productSchedule[0].infoDiscount[0].discount.updateAt,
+						deletedAt:
+							product.productSchedule[0].infoDiscount[0].discount.deletedAt,
+						status: product.productSchedule[0].infoDiscount[0].discount
+							.status,
+					}
+				: undefined;
 		this.createAt = product.createAt;
 		this.updateAt = product.updateAt;
 		this.deletedAt = product.deletedAt;
